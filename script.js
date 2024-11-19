9bea864e6eaf13f477cbd09c21f20223c58249ac
szabobarni <152151212+szabobarni@users.noreply.github.com>
2024-11-19 11:07:13 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function btnEditCountyOnClick(id,name)
-{
-    const countyName = document.getElementById("county_name");
-    countyName.value = name;
-
-    const idCounty = document.getElementById("id_county");
-    idCounty.value = id;
-}
